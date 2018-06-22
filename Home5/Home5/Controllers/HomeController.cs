08b474bfadbbdd5eb2125c87d95f54bccc736652
sirpointer <sirpointer@gmail.com>
2018-06-22 17:38:10 +0400

    Final

--- Home5/Home5/Controllers/HomeController.cs
+++ Home5/Home5/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Home5.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Страница описания.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Контактная информация.";
 
             return View();
         }
